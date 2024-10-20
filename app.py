8484b250f922d07bcfdaff385bb737b4a78ae765
Vickeysvibe <vickeydvk4@gmail.com>
2024-10-20 22:26:01 +0530

    schedules and app cruds added

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from routes.dashboard import dashboard
 from routes.predictions import predictions
 from routes.routes import routes
+from routes.appCrud import appCrud
+from routes.schedule import schedule
 from dotenv import load_dotenv
 import pandas as pd
 import os
@@ -17,7 +19,9 @@ BIN_DATA_FILE = os.path.join(BASE_DIR, 'data', 'garbage_data_check2.csv')
 
 app.register_blueprint(dashboard, url_prefix='/dashboard')
 app.register_blueprint(predictions, url_prefix='/predictions')
+app.register_blueprint(schedule, url_prefix='/schedule')
 app.register_blueprint(routes, url_prefix='/routes')
+app.register_blueprint(appCrud, url_prefix='/appCrud')
 
 
 
